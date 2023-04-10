bc0d44db6dd4bc5f687218f57eb6443e35af5cb4
Your Name <you@example.com>
2023-04-10 16:01:28 -0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,409 +2,320 @@
 
 
 
-/**
- * all music information
- */
+// select all DOM elements
 
-const musicData = [
-  {
-    backgroundImage: "./assets/images/poster-1.jpg",
-    posterUrl: "./assets/images/poster-1.jpg",
-    title: "Happy Moments (Master)",
-    album: "No Spirit",
-    year: 2022,
-    artist: "No Spirit x Tonion",
-    musicPath: "./assets/music/music-1.mp3",
-  },
-  {
-    backgroundImage: "./assets/images/poster-2.jpg",
-    posterUrl: "./assets/images/poster-2.jpg",
-    title: "We Are Going To Be Ok (Master)",
-    album: "No Spirit",
-    year: 2022,
-    artist: "No Spirit x Jhove",
-    musicPath: "./assets/music/music-2.mp3",
-  },
-  {
-    backgroundImage: "./assets/images/poster-3.jpg",
-    posterUrl: "./assets/images/poster-3.jpg",
-    title: "Winter Meadow",
-    album: "No Spirit",
-    year: 2022,
-    artist: "No Spirit x  juniorodeo",
-    musicPath: "./assets/music/music-3.mp3",
-  },
-  {
-    backgroundImage: "./assets/images/poster-4.jpg",
-    posterUrl: "./assets/images/poster-4.jpg",
-    title: "From Where We Started",
-    album: "No Spirit",
-    year: 2022,
-    artist: "No Spirit",
-    musicPath: "./assets/music/music-4.mp3",
-  },
-  {
-    backgroundImage: "./assets/images/poster-5.jpg",
-    posterUrl: "./assets/images/poster-5.jpg",
-    title: "Where I Found You",
-    album: "No Spirit",
-    year: 2022,
-    artist: "No Spirit",
-    musicPath: "./assets/music/music-5.mp3",
-  },
-];
+const headerTime = document.querySelector("[data-header-time]");
+const menuTogglers = document.querySelectorAll("[data-menu-toggler]");
+const menu = document.querySelector("[data-menu]");
+const themeBtns = document.querySelectorAll("[data-theme-btn]");
+const modalTogglers = document.querySelectorAll("[data-modal-toggler]");
+const welcomeNote = document.querySelector("[data-welcome-note]");
+const taskList = document.querySelector("[data-task-list]");
+const taskInput = document.querySelector("[data-task-input]");
+const modal = document.querySelector("[data-info-modal]");
+let taskItem = {};
+let taskRemover = {};
+
+// store current date from build-in date object
+const date = new Date();
+
+// import task complete sound
+const taskCompleteSound = new Audio("./assets/sounds/task-complete.mp3");
 
 
 
 /**
- * add eventListnere on all elements that are passed
+ * convert weekday number to weekday name
+ * totalParameter: 1;
+ * parameterValue: <number> 0-6;
  */
 
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback);
+const getWeekDayName = function (dayNumber) {
+  switch (dayNumber) {
+    case 0:
+      return "Domingo";
+    case 1:
+      return "Lunes";
+    case 2:
+      return "Martes";
+    case 3:
+      return "Miercoles";
+    case 4:
+      return "Jueves";
+    case 5:
+      return "Viernes";
+    case 6:
+      return "Sábado";
+    default:
+      return "No es un día válido";
   }
 }
 
 
 
 /**
- * PLAYLIST
- * 
- * add all music in playlist, from 'musicData'
+ * convert month number to month name
+ * totalParameter: 1;
+ * parameterValue: <number> 0-11;
  */
 
-const playlist = document.querySelector("[data-music-list]");
+const getMonthName = function (monthNumber) {
+  switch (monthNumber) {
+    case 0:
+      return "Jan";
+    case 1:
+      return "Feb";
+    case 2:
+      return "Mar";
+    case 3:
+      return "Apr";
+    case 4:
+      return "May";
+    case 5:
+      return "Jun";
+    case 6:
+      return "Jul";
+    case 7:
+      return "Aug";
+    case 8:
+      return "Sep";
+    case 9:
+      return "Oct";
+    case 10:
+      return "Nov";
+    case 11:
+      return "Dec";
+    default:
+      return "Not a valid month";
+  }
+}
 
-for (let i = 0, len = musicData.length; i < len; i++) {
-  playlist.innerHTML += `
-  <li>
-    <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
-      <img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
-        class="img-cover">
 
-      <div class="item-icon">
-        <span class="material-symbols-rounded">equalizer</span>
-      </div>
+
+// store weekday name, month name & month-of-day number
+const weekDayName = getWeekDayName(date.getDay());
+const monthName = getMonthName(date.getMonth());
+const monthOfDay = date.getDate();
+
+// update headerTime date
+headerTime.textContent = `${weekDayName}, ${monthName} ${monthOfDay}`;
+
+
+
+/**
+ * toggle active class on element
+ * totalParameter: 1;
+ * parameterValue: <object> elementNode;
+ */
+
+const elemToggler = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * toggle active class on multiple elements
+ * totalParameter: 2;
+ * parameterValue: <object> elementNode, <function> any;
+ */
+
+const addEventOnMultiElem = function (elems, event) {
+  for (let i = 0; i < elems.length; i++) {
+    elems[i].addEventListener("click", event);
+  }
+}
+
+
+
+/**
+ * create taskItem elementNode and return it
+ * totalParameter: 1;
+ * parameterValue: <string> any;
+ */
+
+const taskItemNode = function (taskText) {
+
+  const createTaskItem = document.createElement("li");
+  createTaskItem.classList.add("task-item");
+  createTaskItem.setAttribute("data-task-item", "");
+
+  createTaskItem.innerHTML = `
+  
+    <button class="item-icon" data-task-remove="complete">
+      <span class="check-icon"></span>
     </button>
-  </li>
+
+    <p class="item-text">${taskText}</p>
+
+    <button class="item-action-btn" aria-label="Remove task" data-task-remove>
+      <ion-icon name="trash-outline" aria-hidden="true"></ion-icon>
+    </button>
+
   `;
+
+  return createTaskItem;
+
 }
 
 
 
 /**
- * PLAYLIST MODAL SIDEBAR TOGGLE
- * 
- * show 'playlist' modal sidebar when click on playlist button in top app bar
- * and hide when click on overlay or any playlist-item
+ * task input validation
+ * totalParameter: 1;
+ * parameterValue: <string> any
  */
 
-const playlistSideModal = document.querySelector("[data-playlist]");
-const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
-const overlay = document.querySelector("[data-overlay]");
+const taskInputValidation = function (taskIsValid) {
+  if (taskIsValid) {
 
-const togglePlaylist = function () {
-  playlistSideModal.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("modalActive");
+    /**
+     * if there is an existing task
+     * then the new task will be added before it
+     */
+    if (taskList.childElementCount > 0) {
+      taskList.insertBefore(taskItemNode(taskInput.value), taskItem[0]);
+    } else {
+      taskList.appendChild(taskItemNode(taskInput.value));
+    }
+
+    // after adding task on taskList, input field should be empty
+    taskInput.value = "";
+
+    // hide the welcome note
+    welcomeNote.classList.add("hide");
+
+    // update taskItem DOM selection
+    taskItem = document.querySelectorAll("[data-task-item]");
+    taskRemover = document.querySelectorAll("[data-task-remove]");
+
+  } else {
+    // if user pass any falsy value like(0, "", undefined, null, NaN)
+    console.log("Please write something!");
+  }
 }
-
-addEventOnElements(playlistTogglers, "click", togglePlaylist);
 
 
 
 /**
- * PLAYLIST ITEM
- * 
- * remove active state from last time played music
- * and add active state in clicked music
+ * if there is an existing task,
+ * the welcome note will be hidden
  */
 
-const playlistItems = document.querySelectorAll("[data-playlist-item]");
-
-let currentMusic = 0;
-let lastPlayedMusic = 0;
-
-const changePlaylistItem = function () {
-  playlistItems[lastPlayedMusic].classList.remove("playing");
-  playlistItems[currentMusic].classList.add("playing");
+const removeWelcomeNote = function () {
+  if (taskList.childElementCount > 0) {
+    welcomeNote.classList.add("hide");
+  } else {
+    welcomeNote.classList.remove("hide");
+  }
 }
 
-addEventOnElements(playlistItems, "click", function () {
-  lastPlayedMusic = currentMusic;
-  currentMusic = Number(this.dataset.playlistItem);
-  changePlaylistItem();
+
+
+/**
+ * removeTask when click on delete button or check button
+ */
+
+const removeTask = function () {
+
+  // select clicked taskItem
+  const parentElement = this.parentElement;
+
+  /**
+   * if the task is completed, the taskItem would be remove after 250ms
+   * if deleted than taskItem remove instant
+   */
+  if (this.dataset.taskRemove === "complete") {
+
+    parentElement.classList.add("complete"); // add "complete" class on taskItem
+    taskCompleteSound.play(); // play taskCompleteSound
+
+    setTimeout(function () {
+      parentElement.remove(); // remove taskItem
+      removeWelcomeNote(); // remove welcome note
+    }, 250);
+
+  } else {
+    parentElement.remove(); // remove taskItem
+    removeWelcomeNote(); // remove welcom note
+  }
+
+}
+
+
+
+/**
+ * addTask function
+ */
+
+const addTask = function () {
+
+  // check the task inpu validation
+  taskInputValidation(taskInput.value);
+
+  // addEventListere to all taskItem checkbox and delete button
+  addEventOnMultiElem(taskRemover, removeTask);
+
+}
+
+
+
+/**
+ * add keypress listener on taskInput
+ */
+
+taskInput.addEventListener("keypress", function (e) {
+
+  // addTask if user press 'Enter'
+  switch (e.key) {
+    case "Enter":
+      addTask();
+      break;
+  }
+
+});
+
+
+
+// toggle active class on menu when click on menuBtn or dropdownLink 
+const toggleMenu = function () { elemToggler(menu); }
+addEventOnMultiElem(menuTogglers, toggleMenu);
+
+// toggle active class on modal when click on dropdownLink or modal Ok button
+const toggleModal = function () { elemToggler(modal); }
+addEventOnMultiElem(modalTogglers, toggleModal);
+
+
+
+/**
+ * add "loaded" class on body when website is fully loaded
+ */
+
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
 });
 
 
 
 /**
- * PLAYER
- * 
- * change all visual information on player, based on current music
+ * change body background when click on any themeBtn
  */
 
-const playerBanner = document.querySelector("[data-player-banner]");
-const playerTitle = document.querySelector("[data-title]");
-const playerAlbum = document.querySelector("[data-album]");
-const playerYear = document.querySelector("[data-year]");
-const playerArtist = document.querySelector("[data-artist]");
+const themeChanger = function () {
+  // store hue value from clicked themeBtn
+  const hueValue = this.dataset.hue;
 
-const audioSource = new Audio(musicData[currentMusic].musicPath);
+  // create css custom property on root and set value from hueValue
+  document.documentElement.style.setProperty("--hue", hueValue);
 
-const changePlayerInfo = function () {
-  playerBanner.src = musicData[currentMusic].posterUrl;
-  playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
-  document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
-  playerTitle.textContent = musicData[currentMusic].title;
-  playerAlbum.textContent = musicData[currentMusic].album;
-  playerYear.textContent = musicData[currentMusic].year;
-  playerArtist.textContent = musicData[currentMusic].artist;
-
-  audioSource.src = musicData[currentMusic].musicPath;
-
-  audioSource.addEventListener("loadeddata", updateDuration);
-  playMusic();
-}
-
-addEventOnElements(playlistItems, "click", changePlayerInfo);
-
-/** update player duration */
-const playerDuration = document.querySelector("[data-duration]");
-const playerSeekRange = document.querySelector("[data-seek]");
-
-/** pass seconds and get timcode formate */
-const getTimecode = function (duration) {
-  const minutes = Math.floor(duration / 60);
-  const seconds = Math.ceil(duration - (minutes * 60));
-  const timecode = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  return timecode;
-}
-
-const updateDuration = function () {
-  playerSeekRange.max = Math.ceil(audioSource.duration);
-  playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
-}
-
-audioSource.addEventListener("loadeddata", updateDuration);
-
-
-
-/**
- * PLAY MUSIC
- * 
- * play and pause music when click on play button
- */
-
-const playBtn = document.querySelector("[data-play-btn]");
-
-let playInterval;
-
-const playMusic = function () {
-  if (audioSource.paused) {
-    audioSource.play();
-    playBtn.classList.add("active");
-    playInterval = setInterval(updateRunningTime, 500);
-  } else {
-    audioSource.pause();
-    playBtn.classList.remove("active");
-    clearInterval(playInterval);
-  }
-}
-
-playBtn.addEventListener("click", playMusic);
-
-
-/** update running time while playing music */
-
-const playerRunningTime = document.querySelector("[data-running-time");
-
-const updateRunningTime = function () {
-  playerSeekRange.value = audioSource.currentTime;
-  playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-
-  updateRangeFill();
-  isMusicEnd();
-}
-
-
-
-/**
- * RANGE FILL WIDTH
- * 
- * change 'rangeFill' width, while changing range value
- */
-
-const ranges = document.querySelectorAll("[data-range]");
-const rangeFill = document.querySelector("[data-range-fill]");
-
-const updateRangeFill = function () {
-  let element = this || ranges[0];
-
-  const rangeValue = (element.value / element.max) * 100;
-  element.nextElementSibling.style.width = `${rangeValue}%`;
-}
-
-addEventOnElements(ranges, "input", updateRangeFill);
-
-
-
-/**
- * SEEK MUSIC
- * 
- * seek music while changing player seek range
- */
-
-const seek = function () {
-  audioSource.currentTime = playerSeekRange.value;
-  playerRunningTime.textContent = getTimecode(playerSeekRange.value);
-}
-
-playerSeekRange.addEventListener("input", seek);
-
-
-
-/**
- * END MUSIC
- */
-
-const isMusicEnd = function () {
-  if (audioSource.ended) {
-    playBtn.classList.remove("active");
-    audioSource.currentTime = 0;
-    playerSeekRange.value = audioSource.currentTime;
-    playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-    updateRangeFill();
-  }
-}
-
-
-
-/**
- * SKIP TO NEXT MUSIC
- */
-
-const playerSkipNextBtn = document.querySelector("[data-skip-next]");
-
-const skipNext = function () {
-  lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
-  } else {
-    currentMusic >= musicData.length - 1 ? currentMusic = 0 : currentMusic++;
+  // remove "active" class from all themeBtns
+  for (let i = 0; i < themeBtns.length; i++) {
+    if (themeBtns[i].classList.contains("active")) {
+      themeBtns[i].classList.remove("active");
+    }
   }
 
-  changePlayerInfo();
-  changePlaylistItem();
+  // add "active" class on clicked themeBtn
+  this.classList.add("active");
 }
 
-playerSkipNextBtn.addEventListener("click", skipNext);
-
-
-
-/**
- * SKIP TO PREVIOUS MUSIC
- */
-
-const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
-
-const skipPrev = function () {
-  lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
-  } else {
-    currentMusic <= 0 ? currentMusic = musicData.length - 1 : currentMusic--;
-  }
-
-  changePlayerInfo();
-  changePlaylistItem();
-}
-
-playerSkipPrevBtn.addEventListener("click", skipPrev);
-
-
-
-/**
- * SHUFFLE MUSIC
- */
-
-/** get random number for shuffle */
-const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
-
-const shuffleMusic = () => currentMusic = getRandomMusic();
-
-const playerShuffleBtn = document.querySelector("[data-shuffle]");
-let isShuffled = false;
-
-const shuffle = function () {
-  playerShuffleBtn.classList.toggle("active");
-
-  isShuffled = isShuffled ? false : true;
-}
-
-playerShuffleBtn.addEventListener("click", shuffle);
-
-
-
-/**
- * REPEAT MUSIC
- */
-
-const playerRepeatBtn = document.querySelector("[data-repeat]");
-
-const repeat = function () {
-  if (!audioSource.loop) {
-    audioSource.loop = true;
-    this.classList.add("active");
-  } else {
-    audioSource.loop = false;
-    this.classList.remove("active");
-  }
-}
-
-playerRepeatBtn.addEventListener("click", repeat);
-
-
-
-/**
- * MUSIC VOLUME
- * 
- * increase or decrease music volume when change the volume range
- */
-
-const playerVolumeRange = document.querySelector("[data-volume]");
-const playerVolumeBtn = document.querySelector("[data-volume-btn]");
-
-const changeVolume = function () {
-  audioSource.volume = playerVolumeRange.value;
-  audioSource.muted = false;
-
-  if (audioSource.volume <= 0.1) {
-    playerVolumeBtn.children[0].textContent = "volume_mute";
-  } else if (audioSource.volume <= 0.5) {
-    playerVolumeBtn.children[0].textContent = "volume_down";
-  } else {
-    playerVolumeBtn.children[0].textContent = "volume_up";
-  }
-}
-
-playerVolumeRange.addEventListener("input", changeVolume);
-
-
-/**
- * MUTE MUSIC
- */
-
-const muteVolume = function () {
-  if (!audioSource.muted) {
-    audioSource.muted = true;
-    playerVolumeBtn.children[0].textContent = "volume_off";
-  } else {
-    changeVolume();
-  }
-}
-
-playerVolumeBtn.addEventListener("click", muteVolume);
+// add event on all themeBtns
+addEventOnMultiElem(themeBtns, themeChanger);
--- style-guide.md
+++ style-guide.md
@@ -7,13 +7,14 @@ Google font
 ``` html
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 ```
 
-Material icon
+Ionicon
 
 ``` html
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,-25" />
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 ```
 
 ---
@@ -21,73 +22,68 @@ Material icon
 ## Colors
 
 ``` css
---eerie-black_a95: hsla(204, 9%, 11%, 0.95);
---eerie-black_a50: hsla(204, 9%, 11%, 0.5);
---eerie-black: hsl(204, 9%, 11%);
---gainsboro: hsl(225, 7%, 89%);
---charcoal: hsl(203, 9%, 28%);
---silver-sand: hsl(208, 12%, 78%);
---light-sky-blue: hsl(200, 100%, 73%);
---prussian-blue: hsl(196, 100%, 14%);
+--granite-gray: hsl(210, 1%, 37%);
+--eerie-black: hsl(240, 2%, 11%);
+--slimy-green: hsl(91, 100%, 30%);
+--fuzzy-wuzzy: hsl(0, 42%, 57%);
+--davys-gray: hsl(220, 2%, 28%);
+--cultured-1: hsl(228, 17%, 94%);
+--cultured-2: hsl(0, 0%, 96%);
+--dim-gray: hsl(240, 1%, 41%);
+--white_80: hsla(0, 0%, 100%, 0.8);
+--white_70: hsla(0, 0%, 100%, 0.7);
+--white_40: hsla(0, 0%, 100%, 0.4);
+--white_10: hsla(0, 0%, 100%, 0.1);
+--black_05: hsla(0, 0%, 0%, 0.05);
+--black_50: hsla(0, 0%, 0%, 0.5);
+--black_25: hsla(0, 0%, 0%, 0.25);
+--white-2: hsl(0, 0%, 98%);
+--white-1: hsl(0, 0%, 100%);
 --black: hsl(0, 0%, 0%);
---black_a50: hsla(0, 0%, 0%, 0.5);
---black_a20: hsla(0, 0%, 0%, 0.2);
---light-sky-blue_a8: hsla(200, 100%, 73%, 0.08);
---light-sky-blue_a12: hsla(200, 100%, 73%, 0.12);
---silver-sand_a8: hsla(208, 12%, 78%, 0.08);
---silver-sand_a12: hsla(208, 12%, 78%, 0.12);
-
---background: var(--eerie-black);
---background-opacity: var(--eerie-black_a95);
---on-background: var(--gainsboro);
---surface-variant: var(--charcoal);
---on-surface-variant: var(--silver-sand);
---on-surface-variant-hover: var(--light-sky-blue_a8);
---on-surface-variant-focus: var(--light-sky-blue_a12);
---primary: var(--light-sky-blue);
---on-primary: var(--prussian-blue);
 ```
 
-## Gradient color
+## Theme colors
 
 ``` css
---gradient: linear-gradient(180deg, hsla(204, 9%, 11%, 0.90) 60%, transparent 120%);
+--hue: 227;
+--theme-bg: linear-gradient(to bottom, 
+              hsl(var(--hue, 227), 40%, 50%), 
+              hsl(var(--hue, 227), 40%, 60%));
+--theme-color: hsl(var(--hue, 227), 42%, 57%);
 ```
 
 ## Typography
 
 ``` css
---ff-inter: 'Inter', sans-serif;
+--ff-roboto: "Roboto", sans-serif;
 
---headline-sm: 2.4rem;
---title-lg: 2.2rem;
---label-lg: 1.4rem;
---label-md: 1.2rem;
+--fs-1: 2.6rem;
+--fs-2: 2.2rem;
+--fs-3: 1.8rem;
+--fs-4: 1.4rem;
+--fs-5: 1.2rem;
 
---fw-400: 400;
 --fw-500: 500;
+```
+
+## Radius
+
+``` css
+--radius-4: 4px;
+--radius-5: 5px;
+--radius-10: 10px;
 ```
 
 ## Shadow
 
 ``` css
---shadow-1: 0 1px 4px 1px var(--black_a20);
---shadow-2: 0 1px 4px 1px var(--black_a50);
-```
-
-## Border Radius
-
-``` css
---radius-24: 24px;
---radius-16: 16px;
---radius-12: 12px;
---radius-pill: 100px;
---radius-circle: 50%;
+--shadow-1: 0 0 5px hsla(0, 0%, 0%, 0.05);
+--shadow-2: 0 0 10px hsla(0, 0%, 0%, 0.05);
 ```
 
 ## Transition
 
 ``` css
---transition-1: 200ms cubic-bezier(0.2, 0, 0, 1);
---transition-2: 300ms cubic-bezier(0.2, 0, 0, 1);
+--transition: 0.15s ease;
+--cubic-out: cubic-bezier(0.05, 0.65, 0.25, 1);
 ```
